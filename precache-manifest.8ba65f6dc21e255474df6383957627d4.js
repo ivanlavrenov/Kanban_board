a44cc3976d098e31966c91b7a10d82727f3b4403
Ivan Lavrenov <viduck27@gmail.com>
2020-06-17 14:57:46 +0300

    Updates

---
+++ precache-manifest.8ba65f6dc21e255474df6383957627d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dae834c5ea42c278297b83af70b11d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff3b3be81a80611f448a",
+    "url": "/static/css/main.e7d2191c.chunk.css"
+  },
+  {
+    "revision": "cc4d12181381bc8bf1c7",
+    "url": "/static/js/2.c9c40d7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c9c40d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3b3be81a80611f448a",
+    "url": "/static/js/main.648f615b.chunk.js"
+  },
+  {
+    "revision": "57f8cee5cba060c3d0ed",
+    "url": "/static/js/runtime-main.9b244ad5.js"
+  },
+  {
+    "revision": "507fb75a88460fb3b7c1946e0a6925ff",
+    "url": "/static/media/cross.507fb75a.svg"
+  }
+]);
