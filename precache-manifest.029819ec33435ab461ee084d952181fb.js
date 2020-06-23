9d5c13185dfb548837ced91d79fd0c47de5f58d6
Ivan Lavrenov <viduck27@gmail.com>
2020-06-23 13:41:42 +0300

    Updates

---
+++ precache-manifest.029819ec33435ab461ee084d952181fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9047c77670545c0bff6a185e2627ab",
+    "url": "/Kanban_board/index.html"
+  },
+  {
+    "revision": "40d268449f180dc26d51",
+    "url": "/Kanban_board/static/css/main.8841227b.chunk.css"
+  },
+  {
+    "revision": "16bc8545ad06059ac57b",
+    "url": "/Kanban_board/static/js/2.5bdd5637.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Kanban_board/static/js/2.5bdd5637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d268449f180dc26d51",
+    "url": "/Kanban_board/static/js/main.78b1b914.chunk.js"
+  },
+  {
+    "revision": "f3283b1cf8436da83b96",
+    "url": "/Kanban_board/static/js/runtime-main.27b4046b.js"
+  },
+  {
+    "revision": "507fb75a88460fb3b7c1946e0a6925ff",
+    "url": "/Kanban_board/static/media/cross.507fb75a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.32516271bf914c51a4a82d588d56493f.js"
+  "/Kanban_board/precache-manifest.029819ec33435ab461ee084d952181fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Kanban_board/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
